8844dec9ea615a7d37f863c10f27e4ce2e594499
BaamPark <baam.park.95@gmail.com>
2023-11-15 16:39:50 -0500

    latest commit

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbName = process.env.DB_Name;
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Rtssenkp25@@",
-    database: "demodb"
+  host: dbHost,
+  user: dbUser,
+  password: dbPass,
+  database: dbName,
+  port: 3306  // default MySQL port, adjust if yours is different
 });
 
 connection.connect(err => {
